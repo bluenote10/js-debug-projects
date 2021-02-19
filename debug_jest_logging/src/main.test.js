f585a24e185c39df96678c13c81c95db7661fe59
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-02-19 11:31:38 +0100

    minified jest test

--- debug_jest_logging/src/main.test.js
+++ debug_jest_logging/src/main.test.js
@@ -1,83 +1,23 @@
-const logger = require("pino")();
-
 async function simulateRequest() {
   return new Promise((resolve) =>
     setTimeout(() => {
-      logger.info("request...");
+      console.log("request...");
       resolve();
     }, 100)
   );
 }
 
-describe("SomeThing", () => {
-  beforeAll(async () => {
-    logger.info("before all");
-  });
+beforeAll(async () => {
+  console.log("before all");
+});
 
-  afterAll(async () => {
-    logger.info("after all");
-  });
+afterAll(async () => {
+  console.log("after all");
+});
 
-  it("should do 1", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 2", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 3", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 4", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 5", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 6", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
-
-  it("should do 7", async () => {
-    logger.info("before request");
-    console.log("before request");
-    await simulateRequest();
-    logger.info("after request");
-    console.log("after request");
-    expect(true).toBe(true);
-  });
+it("should do 1", async () => {
+  console.log("before request");
+  await simulateRequest();
+  console.log("after request");
+  expect(true).toBe(true);
 });
