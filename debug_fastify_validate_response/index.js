e8eaa9f889f32134aa4b073de1576d80c89d35ed
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-03-18 15:17:11 +0100

    reproduction of validation timeout

---
+++ debug_fastify_validate_response/index.js
@@ -0,0 +1,46 @@
+const fastify = require("fastify")({
+  logger: { prettyPrint: true },
+});
+
+fastify.get(
+  "/",
+  {
+    schema: {
+      query: {
+        properties: { foo: { type: "string" } },
+        required: ["foo"],
+      },
+      response: {
+        200: {
+          properties: { isOk: { type: "boolean" } },
+          required: ["isOk"],
+        },
+        400: {
+          properties: { isOk: { type: "boolean" } },
+          required: ["isOk"],
+        },
+      },
+    },
+  },
+  (request, reply) => {
+    if (Math.random() < 0.5) {
+      reply.send({ isOk: true });
+    } else {
+      reply.code(400).send({ isOk: false });
+    }
+  }
+);
+
+fastify.listen(8080, function (err) {
+  if (err) {
+    throw err;
+  }
+});
+
+/*
+curl http://127.0.0.1:8080\?notfoo\=string
+
+curl --request POST --header "Content-Type: application/json" --data '{ "notfoo": "string" }' http://127.0.0.1:8080
+
+curl --request POST --header "Content-Type: application/json" --data '{ "notfoo": "string" }' --cookie session=OkumyBXNIo7TBvvYLDKVPtyNbVsSe2Td.B2o2Jce%2BU7q8my5sCL2w9oS94O42aFMKjqkRG5Wyafo http://127.0.0.1:8080/api/v1/facts/add
+*/
