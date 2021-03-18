df5bba2353b750e0c3b1b3c56b8e66f74e87653a
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-03-18 21:13:47 +0100

    extended timeout example

--- debug_fastify_validate_response/index.js
+++ debug_fastify_validate_response/index.js
@@ -2,32 +2,55 @@ const fastify = require("fastify")({
   logger: { prettyPrint: true },
 });
 
+const requiresFoo = {
+  properties: { foo: { type: "string" } },
+  required: ["foo"],
+};
+
+const someUserErrorType1 = { type: "string" };
+const someUserErrorType2 = {
+  properties: {
+    code: { type: "number" },
+  },
+  required: ["code"],
+};
+
 fastify.get(
-  "/",
+  "/1",
   {
     schema: {
-      query: {
-        properties: { foo: { type: "string" } },
-        required: ["foo"],
-      },
-      response: {
-        200: {
-          properties: { isOk: { type: "boolean" } },
-          required: ["isOk"],
-        },
-        400: {
-          properties: { isOk: { type: "boolean" } },
-          required: ["isOk"],
-        },
-      },
+      query: requiresFoo,
+      // Not reponse schema here
     },
   },
   (request, reply) => {
-    if (Math.random() < 0.5) {
-      reply.send({ isOk: true });
-    } else {
-      reply.code(400).send({ isOk: false });
-    }
+    reply.code(400).send({ isOk: false });
+  }
+);
+
+fastify.get(
+  "/2",
+  {
+    schema: {
+      query: requiresFoo,
+      response: { 400: someUserErrorType1 },
+    },
+  },
+  (request, reply) => {
+    reply.code(400).send("some error");
+  }
+);
+
+fastify.get(
+  "/3",
+  {
+    schema: {
+      query: requiresFoo,
+      response: { 400: someUserErrorType2 },
+    },
+  },
+  (request, reply) => {
+    reply.code(400).send({ code: 42 });
   }
 );
 
@@ -38,6 +61,8 @@ fastify.listen(8080, function (err) {
 });
 
 /*
+curl 'http://127.0.0.1:8080/1?notfoo=string'
+
 curl http://127.0.0.1:8080\?notfoo\=string
 
 curl --request POST --header "Content-Type: application/json" --data '{ "notfoo": "string" }' http://127.0.0.1:8080
